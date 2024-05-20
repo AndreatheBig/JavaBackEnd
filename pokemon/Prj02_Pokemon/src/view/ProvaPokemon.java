99cb147e7298a7ffa3581193794936f40eeeaba0
AndreatheBig <165818751+AndreatheBig@users.noreply.github.com>
2024-05-20 15:18:49 +0200

    Add files via upload

---
+++ pokemon/Prj02_Pokemon/src/view/ProvaPokemon.java
@@ -0,0 +1,18 @@
+package view;
+
+import controller.PokemonCtrl;
+
+public class ProvaPokemon {
+	public static void main(String[] args) {
+		PokemonCtrl controller = new PokemonCtrl();
+//		controller
+//		.getTipiPokemon()
+//		.forEach(p -> System.out.println(p));
+		
+		controller
+			.getNomiPokemon()
+			.stream()
+			.sorted()
+			.forEach(p -> System.out.println(p));
+	}
+}
